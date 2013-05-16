92471d2df2dd826ab6e45a8c74860443969238f0
Wei Shi <weishi@stanford.edu>
2013-05-16 15:34:09 -0700

    Clean up debug prints in client

--- replFs/client.c
+++ replFs/client.c
@@ -5,8 +5,6 @@
 /* Spring 2013          */
 /************************/
 
-#define DEBUG
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -22,79 +20,41 @@
 
 static ClientInstance *C;
 
-/* ------------------------------------------------------------------ */
-
-int
-InitReplFs( unsigned short portNum, int packetLoss, int numServers )
+int InitReplFs( unsigned short portNum, int packetLoss, int numServers )
 {
-#ifdef DEBUG
-    printf( "InitReplFs: Port number %d, packet loss %d percent, %d servers\n",
-            portNum, packetLoss, numServers );
-#endif
+    PRINT( "InitReplFs: Port number %d, packet loss %d percent, %d servers\n",
+           portNum, packetLoss, numServers );
     C = new ClientInstance(portNum, packetLoss, numServers);
     return( NormalReturn );
 }
 
-/* ------------------------------------------------------------------ */
-
-int
-OpenFile( char *fileName )
+int OpenFile( char *fileName )
 {
-    assert( fileName );
-
-#ifdef DEBUG
-    printf( "OpenFile: Opening File '%s'\n", fileName );
-#endif
-
+    PRINT( "OpenFile: Opening File '%s'\n", fileName );
     return C->openFile(fileName);
 }
 
-/* ------------------------------------------------------------------ */
-
-int
-WriteBlock( int fd, char *buffer, int byteOffset, int blockSize )
+int WriteBlock( int fd, char *buffer, int byteOffset, int blockSize )
 {
-#ifndef DEBUG
-    printf( "WriteBlock: Writing FD=%d, Offset=%d, Length=%d\n",
-            fd, byteOffset, blockSize );
-#endif
     return C->writeBlock(fd, buffer, byteOffset, blockSize);
 }
 
-/* ------------------------------------------------------------------ */
-
-int
-Commit( int fd )
+int Commit( int fd )
 {
-#ifdef DEBUG
-    printf( "Commit: FD=%d\n", fd );
-#endif
+    PRINT( "Commit: FD=%d\n", fd );
     return C->commit(fd);
 
 }
 
-/* ------------------------------------------------------------------ */
-
-int
-Abort( int fd )
+int Abort( int fd )
 {
-#ifdef DEBUG
-    printf( "Abort: FD=%d\n", fd );
-#endif
-
+    PRINT( "Abort: FD=%d\n", fd );
     return C->abort(fd);
 }
 
-/* ------------------------------------------------------------------ */
-
-int
-CloseFile( int fd )
+int CloseFile( int fd )
 {
-
-#ifdef DEBUG
-    printf( "Close: FD=%d\n", fd );
-#endif
-
+    PRINT( "Close: FD=%d\n", fd );
     return C->closeFile(fd);
 }
 
